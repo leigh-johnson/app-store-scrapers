7401fd220710a364a217509cb11ef61553892868
Leigh Johnson <hi@leighjohnson.me>
2018-04-22 23:40:31 -0700

    add keyword scraper redirect view

--- app_store_scrapers/scrapers/urls.py
+++ app_store_scrapers/scrapers/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-
+from django.urls import reverse
 from . import views
 
 app_name = "scrapers"
 urlpatterns = [
     url(regex=r"^$", view=views.KeywordListView.as_view(), name="keyword_list"),
-    url(regex=r"^~create/$", view=views.KeywordCreateView.as_view(), name="keyword_create"),    
+    url(regex=r"^~create/$", view=views.KeywordCreateView.as_view(), name="keyword_create"),
+    url(regex=r"^~popularity_scrape/$", view=views.KeywordPopularyScrapeVie.as_view(), name="keyword_popularity_scrape"),       
     # url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
     # url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
     # url(
--- app_store_scrapers/scrapers/views.py
+++ app_store_scrapers/scrapers/views.py
@@ -1,8 +1,15 @@
+# python
+import random
+# lib
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
-
+from django.views import View
+from django.core.cache import cache
+from django.urls import reverse
+import requests
+# app
 from .models import Keyword
 
 # Create your views here.
@@ -18,4 +25,41 @@ class KeywordCreateView(LoginRequiredMixin, CreateView):
     model = Keyword
     fields = ['text']
     template_name_suffix = '_create_form'
+
+    def get_success_url(self):
+        # if a popularity scrape has been done in the past 24 hours, go straight to detail view
+        text = self.kwargs.get('text')
+        return reverse('scrapers.keyword_popularity_scrape')
+
+
+class KeywordPopularyScrapeView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        cookies = cache.get('cookie:app.searchads.apple.com')
+        csrf_token = cache.get('csrf_token:app.searchads.apple.com')
+
+        url = 'https://app.searchads.apple.com/cm/api/v1/keywords/recommendation'
+
+        query = {
+            'adamId': '1308082090',
+            'storefronts': 'US',
+            'text': keyword
+        }
+
+        headers = {
+            'Cookie': cookies,
+            'X-XSRF-TOKEN-CM': csrf_token,
+            'Host': 'app.searchads.apple.com',
+            'Connection': 'keep-alive',
+            'Accept': 'application/json, text/plain, */*',
+            'apple-request-id': random.randint(1e13, 9e13),
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+            'Referer': 'https://app.searchads.apple.com/cm/app/campaign',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+
+
+
 
